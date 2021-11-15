330d9ed0afb8f52be9dc20f467784b21ad65ea81
Taiki Endo <te316e89@gmail.com>
2021-11-16 00:09:24 +0900

    Update cargo-llvm-cov to 0.1.12

--- Formula/cargo-llvm-cov.rb
+++ Formula/cargo-llvm-cov.rb
@@ -3,19 +3,19 @@
 class CargoLlvmCov < Formula
   desc "Cargo subcommand for LLVM source-based code coverage (-Z instrument-coverage)"
   homepage "https://github.com/taiki-e/cargo-llvm-cov"
-  version "0.1.11"
+  version "0.1.12"
   license any_of: ["Apache-2.0", "MIT"]
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.1.11/cargo-llvm-cov-x86_64-apple-darwin.tar.gz"
-      sha256 "0421276dcffe808a54927b0308bfe0ef1b106bf76c940cf88092b5f171439ad9"
+      url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.1.12/cargo-llvm-cov-x86_64-apple-darwin.tar.gz"
+      sha256 "07e7d886d52c31fec5b5bebfd77fce5d5fd34666543b62a7d326e9dc7aebb68b"
     end
   end
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.1.11/cargo-llvm-cov-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "b262bbc7e600a251ccf937d62fdd01e59f006109c3a6518f30e17dd3b99988d7"
+      url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.1.12/cargo-llvm-cov-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "775ffd8c1bd4ed5dcded78164ec182ca4650a3d98b0700fc1c5daf3b6e33014f"
     end
   end
 
