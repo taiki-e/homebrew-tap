6daa14e269b4dcbc4940ebacaf92a097e9a02096
Taiki Endo <te316e89@gmail.com>
2022-01-04 15:25:16 +0900

    Automatically create PR for formula update

---
+++ ci/formula.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euxo pipefail
+IFS=$'\n\t'
+
+cd "$(cd "$(dirname "$0")" && pwd)"/..
+
+git config user.name "Taiki Endo"
+git config user.email "te316e89@gmail.com"
+
+for formula in Formula/*.rb; do
+    git add -N "${formula}"
+    if ! git diff --exit-code -- "${formula}"; then
+        name="$(basename "${formula%.*}")"
+        version="$(grep <"${formula}" -E '^\s+version "' | sed -E 's/^\s+version "//' | sed 's/"$//')"
+        git add "${formula}"
+        git commit -m "Update ${name} to ${version}"
+        has_update=1
+    fi
+done
+
+if [[ -n "${has_update:-}" ]]; then
+    echo "::set-output name=success::false"
+fi
