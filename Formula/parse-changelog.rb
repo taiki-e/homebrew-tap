fb596c287d3f30f93ccfdb24232a85a1f74a4322
Taiki Endo <te316e89@gmail.com>
2022-10-26 00:17:10 +0000

    Update parse-changelog to 0.5.2

--- Formula/parse-changelog.rb
+++ Formula/parse-changelog.rb
@@ -3,25 +3,25 @@
 class ParseChangelog < Formula
   desc "Simple changelog parser, written in Rust"
   homepage "https://github.com/taiki-e/parse-changelog"
-  version "0.5.1"
+  version "0.5.2"
   license any_of: ["Apache-2.0", "MIT"]
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/taiki-e/parse-changelog/releases/download/v0.5.1/parse-changelog-aarch64-apple-darwin.tar.gz"
-      sha256 "604301690c6d8a104a268a149690fdc819635fc529d0f80cab15871e6548f150"
+      url "https://github.com/taiki-e/parse-changelog/releases/download/v0.5.2/parse-changelog-aarch64-apple-darwin.tar.gz"
+      sha256 "ea4029f555127d082e4f4873ae1382f32b1d0afdaac6952eb346c5ae82c55fee"
     else
-      url "https://github.com/taiki-e/parse-changelog/releases/download/v0.5.1/parse-changelog-x86_64-apple-darwin.tar.gz"
-      sha256 "9fc483f5404131fc4ca68e6b200dfce3248c1eed72656b17258273090ea496fa"
+      url "https://github.com/taiki-e/parse-changelog/releases/download/v0.5.2/parse-changelog-x86_64-apple-darwin.tar.gz"
+      sha256 "3f0df01fe56c3e93747a252c4fa00cd40af53238747c5532665dc5ffea2e0016"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/taiki-e/parse-changelog/releases/download/v0.5.1/parse-changelog-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "1471f2bcdb4ccff7b525335149cd0b1baddc1243c1d7c4f1a093def61e5d3a10"
+      url "https://github.com/taiki-e/parse-changelog/releases/download/v0.5.2/parse-changelog-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "41f4adb16160f2d31df32aebaae4a4966af9d8136b092f0e94d1788ff1c84866"
     else
-      url "https://github.com/taiki-e/parse-changelog/releases/download/v0.5.1/parse-changelog-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "c2591b7921d548f628cb2ce59cd6c0fab1fab44dac62d98a23c75b84d2f2b935"
+      url "https://github.com/taiki-e/parse-changelog/releases/download/v0.5.2/parse-changelog-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "bcaa72cf77aac0b897a8f6380106acb44b1dd28e53feefdd8f91bb4becbf355f"
     end
   end
 
