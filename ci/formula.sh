36d9883878e9f6d2581a35d9ab11e6df876c2676
Taiki Endo <te316e89@gmail.com>
2022-12-09 22:03:35 +0900

    Add spell-check to CI

--- ci/formula.sh
+++ ci/formula.sh
@@ -3,6 +3,9 @@ set -euxo pipefail
 IFS=$'\n\t'
 cd "$(dirname "$0")"/..
 
+# shellcheck disable=SC2154
+trap 's=$?; echo >&2 "$0: Error on line "${LINENO}": ${BASH_COMMAND}"; exit ${s}' ERR
+
 git config user.name "Taiki Endo"
 git config user.email "te316e89@gmail.com"
 
