626da0d661209be65b66d121ff605d14fdecd4e9
Taiki Endo <te316e89@gmail.com>
2023-04-03 01:53:51 +0000

    Update cargo-llvm-cov to 0.5.12

--- Formula/cargo-llvm-cov.rb
+++ Formula/cargo-llvm-cov.rb
@@ -3,25 +3,25 @@
 class CargoLlvmCov < Formula
   desc "Cargo subcommand for LLVM source-based code coverage (-C instrument-coverage)"
   homepage "https://github.com/taiki-e/cargo-llvm-cov"
-  version "0.5.11"
+  version "0.5.12"
   license any_of: ["Apache-2.0", "MIT"]
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.5.11/cargo-llvm-cov-aarch64-apple-darwin.tar.gz"
-      sha256 "276d8fa70299ec9ae014cab905cdbd614d6bddfa6abcc414492091585dd55ab5"
+      url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.5.12/cargo-llvm-cov-aarch64-apple-darwin.tar.gz"
+      sha256 "05c8f8e2210a3218b167dce33477785a0bc0c718fac7b15d2f4a428791234f03"
     else
-      url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.5.11/cargo-llvm-cov-x86_64-apple-darwin.tar.gz"
-      sha256 "fe0e54836a367697a1b475440af2e7438e56caf6e548b88faa88edc221a515f7"
+      url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.5.12/cargo-llvm-cov-x86_64-apple-darwin.tar.gz"
+      sha256 "77faedad91c300b51d2d40040a6c61903a555da319047983ec56c81598cbea9b"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.5.11/cargo-llvm-cov-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "479f51fac713151f73ee0ab5d7cb1155646cde17903dfd7c526cd51253e99a4c"
+      url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.5.12/cargo-llvm-cov-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "87c54166055d2d486620ceb27440442d34d71b364fdd6f6da57a210ae5f75e33"
     else
-      url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.5.11/cargo-llvm-cov-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "3bd355e29b8e29d0b42f0c95ec487cd8e00ffa419ba65b83968740a641a46c1e"
+      url "https://github.com/taiki-e/cargo-llvm-cov/releases/download/v0.5.12/cargo-llvm-cov-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "998b1d9e630295459743fe7051a7e3d04e82836456171493eb5983dde710b07e"
     end
   end
 
