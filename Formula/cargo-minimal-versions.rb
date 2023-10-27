8b500fc0bb038bad902f3048b38a80cbc2b23ac0
Taiki Endo <te316e89@gmail.com>
2023-10-27 15:50:41 +0000

    Update cargo-minimal-versions to 0.1.21

--- Formula/cargo-minimal-versions.rb
+++ Formula/cargo-minimal-versions.rb
@@ -4,25 +4,25 @@
 class CargoMinimalVersions < Formula
   desc "Cargo subcommand for proper use of -Z minimal-versions"
   homepage "https://github.com/taiki-e/cargo-minimal-versions"
-  version "0.1.20"
+  version "0.1.21"
   license any_of: ["Apache-2.0", "MIT"]
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/taiki-e/cargo-minimal-versions/releases/download/v0.1.20/cargo-minimal-versions-aarch64-apple-darwin.tar.gz"
-      sha256 "376c20fcf46134a3e006d7b2737521a89e75a39b032868f479755931bb3ae397"
+      url "https://github.com/taiki-e/cargo-minimal-versions/releases/download/v0.1.21/cargo-minimal-versions-aarch64-apple-darwin.tar.gz"
+      sha256 "0a86d29bb4c43e54e0836aa12fe6955c2bc33c1aaef8f0b6f04f2cde24786a65"
     else
-      url "https://github.com/taiki-e/cargo-minimal-versions/releases/download/v0.1.20/cargo-minimal-versions-x86_64-apple-darwin.tar.gz"
-      sha256 "4dfce639a978a913ef374125a8721f7fb551238a861863865244b97742c936af"
+      url "https://github.com/taiki-e/cargo-minimal-versions/releases/download/v0.1.21/cargo-minimal-versions-x86_64-apple-darwin.tar.gz"
+      sha256 "783d3e662e6ad584ba2230ff427debbd6e2dcccb3bd1d1a74ca6dcab1a761830"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/taiki-e/cargo-minimal-versions/releases/download/v0.1.20/cargo-minimal-versions-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "9810227752801c9770893613b193db780916a990fb7c8490e55f0ba8352d4d03"
+      url "https://github.com/taiki-e/cargo-minimal-versions/releases/download/v0.1.21/cargo-minimal-versions-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "a21e1bf829b433b86c8bf77da51e300bb2dd06e563c10bae848043f5a6f6d8c8"
     else
-      url "https://github.com/taiki-e/cargo-minimal-versions/releases/download/v0.1.20/cargo-minimal-versions-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "b6bc98e33c7a2868d8d2079fbc0bb4fab13e7b61927242acdade67901eb8a5d1"
+      url "https://github.com/taiki-e/cargo-minimal-versions/releases/download/v0.1.21/cargo-minimal-versions-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "2fe86915ae5f67a00b4ca0f94f98d57036ed549dba2247d8c20be178d3bd33f2"
     end
   end
 
