804b310021a6dfd094d69471aaf7d73d519beec3
Taiki Endo <te316e89@gmail.com>
2024-02-21 06:05:40 +0000

    Update cargo-hack to 0.6.20

--- Formula/cargo-hack.rb
+++ Formula/cargo-hack.rb
@@ -4,25 +4,25 @@
 class CargoHack < Formula
   desc "Cargo subcommand for testing and continuous integration"
   homepage "https://github.com/taiki-e/cargo-hack"
-  version "0.6.19"
+  version "0.6.20"
   license any_of: ["Apache-2.0", "MIT"]
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/taiki-e/cargo-hack/releases/download/v0.6.19/cargo-hack-aarch64-apple-darwin.tar.gz"
-      sha256 "5d2e027a34ab26949534e5e9a764c49130515bb4eaff7e4b1e824ace6ba10617"
+      url "https://github.com/taiki-e/cargo-hack/releases/download/v0.6.20/cargo-hack-aarch64-apple-darwin.tar.gz"
+      sha256 "59ac8f0227168a05a758e6fcdaa7fb7b91bf9d206fcf19644f4a15b864842c13"
     else
-      url "https://github.com/taiki-e/cargo-hack/releases/download/v0.6.19/cargo-hack-x86_64-apple-darwin.tar.gz"
-      sha256 "277fdb2c362fad398cc858581cf06b1d3706cbbf883005285f3794ba5aa2f1f1"
+      url "https://github.com/taiki-e/cargo-hack/releases/download/v0.6.20/cargo-hack-x86_64-apple-darwin.tar.gz"
+      sha256 "c8a6bcf8f48761da905dbb476a2c276f1b3beb4e601d895734196a7f33cb20fc"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/taiki-e/cargo-hack/releases/download/v0.6.19/cargo-hack-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "aa24c54f97b43685979ee15f2a03b0a249cc035449a3a1955e64c61e725a9e8c"
+      url "https://github.com/taiki-e/cargo-hack/releases/download/v0.6.20/cargo-hack-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "3ac4d20c14d73c5ec87ac6132f3083f419340f4f124b31f55594a9d364f1eb11"
     else
-      url "https://github.com/taiki-e/cargo-hack/releases/download/v0.6.19/cargo-hack-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "c96cbde6b2eed3ba5c3a62911bd729362dad11ee2363cefb79e7d8b44c034b25"
+      url "https://github.com/taiki-e/cargo-hack/releases/download/v0.6.20/cargo-hack-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "dc3a790ba52a824f3faeb45bf630b35ed4cfe2ef5de24e169bd1672c40ca0ad0"
     end
   end
 
